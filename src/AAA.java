e5f7ba08ee1231cb65d9a28766d7e5686c4c563d
sung <sungchan@vendys.co.kr>
2020-11-16 10:34:13 +0900

    super, override

---
+++ src/AAA.java
@@ -0,0 +1,9 @@
+public class AAA {
+    public static void main(String[] args) {
+        Person name_1 = new Person();
+
+        System.out.println(name_1.name);
+
+        System.out.println(name_1.print());
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,32 @@
+class Person {
+    String name;
+    int age = 20;
+
+    public Person() {
+        this("이름");
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+    public Person print() {
+        return this;
+    }
+
+    public void BBB() {
+        System.out.println(name);
+    }
+}
+
+class newPerson extends Person {
+    int age = 18;
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public void BBB() {
+        System.out.println(age);
+    }
+}
